7eea5f01a51bfecae09097f7d52fb78ffe43be98
Sathsara33 <aheroku72@gmail.com>
2025-03-28 22:36:28 -0700

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -22,7 +22,7 @@ const config = {
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "private",
   OWNER_NAME: process.env.OWNER_NAME || "BABIYA-MD",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "94764978991",
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
 };
